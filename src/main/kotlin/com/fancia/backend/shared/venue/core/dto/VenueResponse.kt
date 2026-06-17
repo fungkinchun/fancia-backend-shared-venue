82163ed5e4e0b8bf66ad684c3476cd18ab9edf13
fungkinchun <fungkinchun@gmail.com>
2026-06-17 16:07:52 +0100

    first commit

--- src/main/kotlin/com/fancia/backend/shared/venue/core/dto/VenueResponse.kt
+++ src/main/kotlin/com/fancia/backend/shared/venue/core/dto/VenueResponse.kt
@@ -2,7 +2,6 @@ package com.fancia.backend.shared.venue.core.dto
 
 import com.fancia.backend.shared.common.location.core.dto.LocationDto
 import com.fancia.backend.shared.common.social.core.dto.LinkResponse
-import com.fancia.backend.shared.common.tag.core.dto.TagResponse
 import java.time.LocalDateTime
 import java.util.*
 
@@ -12,7 +11,7 @@ data class VenueResponse(
     var description: String = "",
     var createdBy: UUID? = null,
     var createdAt: LocalDateTime? = null,
-    var tags: Set<TagResponse> = emptySet(),
+    var tags: Set<UUID> = emptySet(),
     var links: Set<LinkResponse> = emptySet(),
     var location: LocationDto? = null,
 )
